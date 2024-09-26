7830d465af74acafef311f77e6132810ac86fefb
zubair mallik <https://github.com/zubair-mallick/format_data.git>
2024-09-26 20:14:30 +0530

    add comit

--- fsipd/frontend/src/components/Councellinng.jsx
+++ fsipd/frontend/src/components/Councellinng.jsx
@@ -4,7 +4,7 @@ import ResourceFinder from './ResourceFinder';
 
 const CounselingPage = () => {
   return (
-    <div id='counseling' className="p-8 min-h-screen ">
+    <div id='counseling' className="p-8 min-h-screen">
       <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-900 mb-6">
         Counseling and Resource Guidance
       </h1>
@@ -17,7 +17,7 @@ const CounselingPage = () => {
           <p>Enter your desired career name to receive detailed information about the relevant exams, scholarships, prerequisites, and programs. This tool helps you understand the steps needed to pursue your career goals effectively.</p>
         </li>
         <li>
-          <strong className="text-blue-400 mb-2">Resource Finder:</strong> 
+          <strong className  ="text-blue-400 mb-2">Resource Finder:</strong> 
           <p>Input an exam or technology to find a curated list of resources, including groups, communities, and old question papers. This tool connects you with valuable resources to aid your preparation and learning journey.</p>
         </li>
       </ul>
