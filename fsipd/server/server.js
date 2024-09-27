11d12881cc481fd5f4af440861d3d450f106e310
zubair mallik <zubair7035@gmail.com>
2024-09-27 13:02:26 +0530

    allow cors acess

--- fsipd/server/server.js
+++ fsipd/server/server.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 
